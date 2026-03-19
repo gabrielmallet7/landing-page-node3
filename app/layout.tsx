66c6930f74308cc8b06f89ddc7b7b8dfd12ad5d8
gabrielmallet7 <mallet.gabriel2@gmail.com>
2026-03-19 01:59:52 -0300

    final

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,21 +20,8 @@ export const metadata: Metadata = {
   description: 'Soluciones reales para problemas reales.',
 
   icons: {
-    icon: [
-      {
-        url: '/icon-light-32x32.png',
-        media: '(prefers-color-scheme: light)',
-      },
-      {
-        url: '/icon-dark-32x32.png',
-        media: '(prefers-color-scheme: dark)',
-      },
-      {
-        url: '/icon.svg',
-        type: 'image/svg+xml',
-      },
-    ],
-    apple: '/apple-icon.png',
+    icon: '/logos/node3-fondo.jpeg',
+    apple: '/logos/node3-fondo.jpeg',
   },
 
   openGraph: {
